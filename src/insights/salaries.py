006e0335f656de132fd4f1c83fb1bda61ef1c4b8
Renê Alves <renepvh11@gmail.com>
2022-12-08 16:43:37 -0400

    feat: trazendo menor salario

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -15,21 +15,14 @@ def get_max_salary(path: str) -> int:
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    jobs = read(path)
+    return min(
+        [
+            int(job["min_salary"])
+            for job in jobs
+            if (job["min_salary"]).isdigit()
+        ]
+    )
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
