315e0c0f8fa82ec53540537acc5b5d212be19dd4
Renê Alves <renepvh11@gmail.com>
2022-12-08 14:37:27 -0400

    feat: listando as industries que nao estao com o campo vazio

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,15 @@
-from typing import List, Dict
+from typing import Dict, List
+
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    jobs = read(path)
+    jobs_list_types = []
+    for job in jobs:
+        if job["industry"] != "":
+            jobs_list_types.append(job["industry"])
+    return list(set(jobs_list_types))
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
