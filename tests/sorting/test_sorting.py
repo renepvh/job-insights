26f45d46123f8a4a9094d4f2801c572610c997f4
Renê Alves <renepvh11@gmail.com>
2022-12-09 18:56:58 -0400

    test: implementando o test da função sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,57 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs_list = [
+        {
+            "title": "Maquinista",
+            "max_salary": "2000",
+            "min_salary": "1800",
+            "date_posted": "2021-11-29",
+        },
+        {
+            "title": "Motorista",
+            "max_salary": "1900",
+            "min_salary": "1500",
+            "date_posted": "2021-12-29",
+        },
+        {
+            "title": "Ux Designer",
+            "max_salary": "2500",
+            "min_salary": "1800",
+            "date_posted": "2021-10-29",
+        },
+        {
+            "title": "Gerente Comercial",
+            "max_salary": "3500",
+            "min_salary": "2950",
+            "date_posted": "2021-09-29",
+        },
+    ]
+    sort_by(jobs_list, "max_salary")
+    assert jobs_list == [
+        {
+            "title": "Gerente Comercial",
+            "max_salary": "3500",
+            "min_salary": "2950",
+            "date_posted": "2021-09-29",
+        },
+        {
+            "title": "Ux Designer",
+            "max_salary": "2500",
+            "min_salary": "1800",
+            "date_posted": "2021-10-29",
+        },
+        {
+            "title": "Maquinista",
+            "max_salary": "2000",
+            "min_salary": "1800",
+            "date_posted": "2021-11-29",
+        },
+        {
+            "title": "Motorista",
+            "max_salary": "1900",
+            "min_salary": "1500",
+            "date_posted": "2021-12-29",
+        },
+    ]
